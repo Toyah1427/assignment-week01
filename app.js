22b2d830e6eda3ac9c5bd18900b0c9a50399b2b7
toyah1427 <toyahdegroat1427@gmail.com>
2024-03-22 13:33:31 +0000

    basic site

---
+++ app.js
@@ -0,0 +1,8 @@
+let themeAnswer = prompt("Are your eyes sore? Select if you want a black or white theme");
+
+if (themeAnswer === "white") {
+    document.body.style.backgroundColor = themeAnswer;
+} else if (themeAnswer === "black") {
+    document.body.style.backgroundColor = themeAnswer;
+}
+
